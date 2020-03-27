4fc2662fc1d7a1936f409d22cd3aecf62bbdfbcd
fengbin <27317142@qq.com>
2020-03-28 00:13:15 +0800

    init pro

---
+++ js/youtube_control.js
@@ -0,0 +1,14 @@
+scheduleAutoCloseAdv()
+function scheduleAutoCloseAdv () {
+    setInterval(() => {
+        close_adv()
+    }, 1000)
+}
+function close_adv () {
+    let btns = document.getElementsByClassName('ytp-ad-skip-button-icon')
+    if (btns.length !== 1) {
+        return
+    }
+    btns[0].click();
+    console.info('click ok')
+}
